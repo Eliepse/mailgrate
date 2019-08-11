fa40765719bbe4530e0b19042e43d45c21a34d6f
Eliepse <contact@eliepse.fr>
2019-08-11 12:49:04 +0200

    Clean command and test

---
+++ tests/Feature/Commands/CreateAccountCommandTest.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Tests\Feature\Commands;
+
+
+use Tests\TestCase;
+
+class CreateAccountCommandTest extends TestCase
+{
+    public function testNoErrors()
+    {
+//        $this->artisan('account:create')
+//            ->expectsQuestion("What is the host domain?", "imap.test.local")
+//            ->expectsQuestion("What is the port?", null)
+//            ->expectsQuestion("What is the security protocol?", null)
+//            ->expectsQuestion("What is the username?", "j.doe@exemple.com");
+        $this->markTestIncomplete("Should find a way to mock imap server.");
+    }
+}
