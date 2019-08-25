3538ce093867ecd3fefcd8352dc6feac62dfc418
Eliepse <contact@eliepse.fr>
2019-08-25 17:09:13 +0200

    feat: account management trait

---
+++ src/Imap/Actions/AccountManagement.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace Eliepse\Imap\Actions;
+
+
+use App\Account;
+use Eliepse\Imap\AccountPasswordManager;
+
+trait AccountManagement
+{
+    protected function getAccountFromModel(Account $account): ?Account
+    {
+        return app(AccountPasswordManager::class)->get($account->id);
+    }
+
+
+    protected function getAccountFromId(int $id): ?Account
+    {
+        return app(AccountPasswordManager::class)->get($id);
+    }
+
+
+    /**
+     * @param int $id
+     *
+     * @return bool
+     */
+    protected function accountHasPassword(int $id): bool
+    {
+        return !empty($this->getAccountFromId($id)->password);
+    }
+}
