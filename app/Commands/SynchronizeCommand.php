333c688541acc22a2812f99b8c26e7966b030628
Eliepse <contact@eliepse.fr>
2019-09-01 14:41:41 +0200

    fix: attempt to fix non-sync folders

--- app/Commands/SynchronizeCommand.php
+++ app/Commands/SynchronizeCommand.php
@@ -99,6 +99,7 @@ class SynchronizeCommand extends Command
         $this->info("\nCopying folder structure...");
         (new CopyAccountFolderStructureAction($this->output, $this->from->id))($this->to->id);
 
+        $this->from->load(['folders.mails']);
         $this->to->load(['folders']);
 
         $this->info("\nCopying mails...");
